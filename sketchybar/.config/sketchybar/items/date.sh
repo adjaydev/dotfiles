702e03e4f2c06b50974303f2ab10c4725cd15d69
adjaydev <contact@adjay.dev>
2024-11-25 18:05:33 +0100

    Updated date

--- sketchybar/.config/sketchybar/items/date.sh
+++ sketchybar/.config/sketchybar/items/date.sh
@@ -2,25 +2,41 @@
 
 
 sketchybar --add item time right \
-  --set time update_freq=20 label="󱑎 $(date "+%H:%M")u." \
-  padding_right=20    \
+  --set time update_freq=20 label="$(date '+%-H:%M')u." \
+  padding_right=20 \
   label.color=$GREEN
 
+sketchybar --add item timeIcon right \
+  --set timeIcon label=" " \
+  padding_right=10    \
+  label.color=$GREEN
 
-sketchybar --add item wkdate right \
-  --set wkdate update_freq=3600 label="$(date "+%d-%m")" \
-  padding_right=20 \
-  label.color=$BLUE
-
-
-sketchybar --add item wkdateicon right \
-  --set wkdateicon update_freq=0 label="󰃭 " \
-  padding_right=5 \
-  label.color=$BLUE
-
-
-sketchybar --add item wk right \
-  --set wk update_freq=0 label="wk $(date "+%W")" \
+sketchybar --add item date right \
+  --set date update_freq=20 label="$(date '+%a %-d %b')" \
   padding_right=10 \
   label.color=$BLUE
 
+sketchybar --add item wkdateicon right \
+  --set wkdateicon update_freq=0 label="wk $(date "+%W")" \
+  padding_right=5 \
+  label.color=$BLUE
+
+sketchybar --add item dateIcon right \
+  --set dateIcon label="󰃮 " \
+  padding_right=10    \
+  label.color=$BLUE
+
+
+# sketchybar --add item wkdate right \
+#   --set wkdate update_freq=3600 label="$(date "+%d-%m")" \
+#   padding_right=20 \
+#   label.color=$BLUE
+#
+#
+#
+#
+# sketchybar --add item wk right \
+#   --set wk update_freq=0 label="wk $(date "+%W")" \
+#   padding_right=10 \
+#   label.color=$BLUE
+#
