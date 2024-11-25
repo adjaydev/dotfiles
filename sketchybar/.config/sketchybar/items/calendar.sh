5351d7022259c1731b6f714593e76b39d4e56fb3
Adjay Thakoerdien <Adjay@Mac.home>
2024-11-25 16:14:15 +0100

    Updated sjetchybar styling

--- sketchybar/.config/sketchybar/items/calendar.sh
+++ sketchybar/.config/sketchybar/items/calendar.sh
@@ -3,12 +3,13 @@
 
 sketchybar --add item calendarEvent right \
     --set calendarEvent update_freq=60 \
-    label="$(icalbuddy -n -nc -b "" -iep "title" -po "title" -tf "%H:%M" -df "%Y-%m-%d" eventsToday | head -n 1)" \
-    padding_right=20
+    label="$(icalbuddy -n -nc -b "" -iep "title" -po "title" -tf "%H:%M" -df "%Y-%m-%d" eventsToday+1 | head -n 1)" \
+    padding_right=20 \
+    label.color=$GREY
 
 
 sketchybar --add item calendarTime right \
     --set calendarTime update_freq=60 \
-    label="$(icalbuddy -n -nc -b "" -iep "datetime" -po "datetime" -tf "%H:%M" -df "%Y-%m-%d" eventsToday | head -n 1)" \
-    label.color=$GREEN \
-    padding_right=20
+    label="󰃭 $(icalbuddy -n -nc -b "" -iep "datetime" -po "datetime" -tf "%H:%M" -df "%Y-%m-%d" eventsToday+1 | head -n 1):" \
+    padding_right=10 \
+    label.color=$GREY
--- sketchybar/.config/sketchybar/items/date.sh
+++ sketchybar/.config/sketchybar/items/date.sh
@@ -2,23 +2,25 @@
 
 
 sketchybar --add item time right \
-  --set time update_freq=20 script='sketchybar --set $NAME label="󱑎 $(date "+%H:%M")u."'\
+  --set time update_freq=20 label="󱑎 $(date "+%H:%M")u." \
   padding_right=20    \
   label.color=$GREEN
 
 
 sketchybar --add item wkdate right \
-  --set wkdate update_freq=3600 script='sketchybar --set $NAME label=" $(date "+%d-%m") -"'\
-  padding_right=10
+  --set wkdate update_freq=3600 label="$(date "+%d-%m")" \
+  padding_right=20 \
+  label.color=$BLUE
 
 
 sketchybar --add item wkdateicon right \
-  --set wkdateicon update_freq=0 script='sketchybar --set $NAME label="󱨰 "'\
-  padding_right=0 \
-  label.color=$RED
+  --set wkdateicon update_freq=0 label="󰃭 " \
+  padding_right=5 \
+  label.color=$BLUE
 
 
 sketchybar --add item wk right \
-  --set wk update_freq=0 script='sketchybar --set $NAME label="wk $(date "+%W")"'\
-  padding_right=10
+  --set wk update_freq=0 label="wk $(date "+%W")" \
+  padding_right=10 \
+  label.color=$BLUE
 
