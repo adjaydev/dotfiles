5a24d666a6867246f97ce40f936e4070a205a270
Adjay Thakoerdien <Adjay@MBP-van-ladmin.home>
2024-11-26 08:35:10 +0100

    Cleanup

--- sketchybar/.config/sketchybar/plugins/teams.sh
+++ sketchybar/.config/sketchybar/plugins/teams.sh
@@ -22,27 +22,6 @@ elif [[ $LABEL =~ ^[0-9]+$ ]]; then
     ICON_COLOR="0xffed8796"
 else
     exit 0
-    # ICON_COLOR="0xffa6da95" # Default to green
 fi
 
 sketchybar --set $NAME icon=$ICON label="${LABEL}" icon.color=${ICON_COLOR} icon.padding_right=10
-
-# if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
-#     LABEL="${BASH_REMATCH[1]}"
-#     echo $LABEL
-#
-#     if [[ $LABEL == "" ]]; then
-#         ICON_COLOR="0xffa6da95"
-#     elif [[ $LABEL == "•" ]]; then
-#         ICON_COLOR="0xffeed49f"
-#     elif [[ $LABEL =~ ^[0-9]+$ ]]; then
-#         ICON_COLOR="0xffed8796"
-#     else
-#         ICON_COLOR="0xffa6da95"
-#         exit 0
-#     fi
-# else
-#   exit 0
-# fi
-#
-# sketchybar --set $NAME icon=$ICON label="${LABEL}" icon.color=${ICON_COLOR} icon.padding_right=10
