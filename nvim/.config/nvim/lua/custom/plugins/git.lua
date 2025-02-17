df1dab018bca992c3235577f81c939e2656853b0
adjaythakoerdien <a.thakoerdien@gmail.com>
2025-02-17 13:26:24 +0100

    updated git stuff

--- nvim/.config/nvim/lua/custom/plugins/git.lua
+++ nvim/.config/nvim/lua/custom/plugins/git.lua
@@ -1,6 +1,9 @@
 return {
   {
     'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', '<leader>gg', ':G<CR>', {})
+    end,
   },
   {
     'lewis6991/gitsigns.nvim',
@@ -8,6 +11,7 @@ return {
       require('gitsigns').setup()
 
       vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
+      vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
     end,
   },
 }
