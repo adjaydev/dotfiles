c561796dc53060c44b7dd03bbf77aa8e5b7b0705
adjaydev <contact@adjay.dev>
2025-02-25 18:58:53 +0100

    update ts gymdev

--- tmf/.config/tmuxifier/layouts/gymdev.session.sh
+++ tmf/.config/tmuxifier/layouts/gymdev.session.sh
@@ -1,14 +1,16 @@
 session_root "~/projects/gymdev"
 if initialize_session "gymdev"; then
   new_window "frontend"
-  new_window "backend"
   new_window "ng server"
+  new_window "backend"
   new_window "go server"
   new_window "zsh"
-  select_window 3
-  run_cmd "ng serve --port 4300"
   select_window 2
-  run_cmd "cd backend; "
+  run_cmd "cd frontend; ng serve --port 4300"
+  select_window 4
+  run_cmd "cd backend; go run ."
+  select_window 3
+  run_cmd "cd backend; v."
   select_window 1
   run_cmd "cd frontend; v."
 fi
