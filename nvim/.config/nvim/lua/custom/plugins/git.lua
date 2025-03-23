4acd9571905c356fe5e429822660be73e0caf231
adjaydev <contact@adjay.dev>
2025-03-23 17:39:22 +0100

    update

--- nvim/.config/nvim/lua/custom/plugins/git.lua
+++ nvim/.config/nvim/lua/custom/plugins/git.lua
@@ -5,6 +5,7 @@ return {
       vim.keymap.set('n', '<leader>gg', ':G<CR>', {})
       vim.keymap.set('n', '<leader>gb', ':G blame<CR>', {})
       vim.keymap.set('n', '<leader>gp', ':G push<CR>', {})
+      vim.keymap.set('n', '<leader>gs', ':G switch -c', {})
       vim.keymap.set('n', '<leader>gc', ':Telescope git_bcommits<CR>', {})
     end,
   },
