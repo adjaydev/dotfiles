333c6f4ce376f98988154418eaaf8cc2b16bc9e3
adjaythakoerdien <a.thakoerdien@gmail.com>
2025-03-26 12:32:37 +0100

    update

--- tmf/.config/tmuxifier/layouts/bvsn.session.sh
+++ tmf/.config/tmuxifier/layouts/bvsn.session.sh
@@ -2,7 +2,10 @@ session_root "~/projects/bvcm-synapse-backend/"
 if initialize_session "bvsn"; then
   new_window "editor"
   new_window "server"
+  new_window "lazygit"
   new_window "zsh"
+  select_window 3
+  run_cmd "lzg"
   select_window 2
   run_cmd "poetry run uvicorn main:app --port 6767 --reload"
   select_window 1
