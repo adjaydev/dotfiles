a6e4f7c34befe31c6525fbe897c71809cebfdc77
adjaydev <contact@adjay.dev>
2025-03-26 19:00:27 +0100

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -196,8 +196,21 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<Leader>sa', ':lua require"telescope.builtin".find_files({ hidden = true })<CR>', { noremap = true, silent = true })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<Leader>sa', function()
+        require('telescope.builtin').find_files {
+          hidden = true,
+          find_command = {
+            'rg',
+            '--files',
+            '--hidden',
+            '--glob',
+            '!**/.git/*',
+            '--glob',
+            '!**/.idea/*',
+          },
+        }
+      end, { noremap = true, silent = true })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
