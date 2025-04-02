06b612f59a25352045eae3925f839b90d8bf9b84
adjaythakoerdien <a.thakoerdien@gmail.com>
2025-04-02 11:44:44 +0200

    update

--- tmf/.config/tmuxifier/layouts/dev-heiloo.session.sh
+++ tmf/.config/tmuxifier/layouts/dev-heiloo.session.sh
@@ -2,10 +2,10 @@ session_root "~/projects/dev-heiloo"
 if initialize_session "dev-heiloo"; then
   new_window "nvim"
   new_window "server"
-  new_window "zsh"
+  new_window "lazygit"
   new_window "db"
-  # select_window 4
-  # run_cmd "v."
+  select_window 3
+  run_cmd "lazygit"
   select_window 2
   run_cmd "poetry run uvicorn main:app --reload --port 6543"
   select_window 1
