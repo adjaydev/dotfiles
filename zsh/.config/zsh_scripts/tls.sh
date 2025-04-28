aa965e2402dda71fa597e8fe759d77e772dfb2f0
adjaythakoerdien <a.thakoerdien@gmail.com>
2025-04-28 08:27:48 +0200

    updated tmux

--- zsh/.config/zsh_scripts/tls.sh
+++ zsh/.config/zsh_scripts/tls.sh
@@ -1,6 +1,14 @@
 #! /usr/bin/env sh
 
+tmux info > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+	echo "[no tmux server running]"
+	exit 1
+fi
+
 selected=`tmux ls | sort -r | fzf | cut -d ':' -f 1`
+
 if [[ -n $selected ]]; then
 	tmux attach -t $selected
 else
